601bc980e87a75d60aced905fb7c19a4434c08b2
AdryDevs <alonsoadrian86@gmail.com>
2022-10-03 19:15:51 +0200

    ejercicios comienzo JS

---
+++ js/main.js
@@ -0,0 +1,66 @@
+
+// var number = 4;
+// var boolean = false;
+// var string = "soy un string";
+
+// console.log(string +" + " + boolean + " + " + number);
+
+// console.log(typeof(string));  // indica de qué tipo es la variable
+
+// var prueba = "letras";
+
+// console.log(prueba++);
+
+
+
+// edad = prompt("Cuántos años tienes?");
+
+// if (edad >= 18){
+//     console.log("Puedes ver la película");
+// } else {
+//     console.log("No puedes ver la película");
+//     }
+
+
+// var mes = "";
+
+// switch (mes) {
+//     case "Enero":
+//     case "Febrero":
+//     case "Diciembre":
+
+//         console.log("Es invierno")
+        
+//         break;
+
+//     case "Marzo":
+//     case "Abril":
+//     case "Mayo":
+//     case "Junio":
+
+//         console.log("Es primavera")
+        
+//         break;
+
+//     case "Julio":
+//     case "Agosto":
+
+//         console.log("Es verano")
+        
+//         break;
+
+//     case "Septiembre":
+//     case "Octubre":
+//     case "Noviembre":
+
+//         console.log("Es otoño")
+        
+//         break;
+    
+
+//     default:
+
+//         console.log("No has introducido un mes");
+
+//         break;
+// }
